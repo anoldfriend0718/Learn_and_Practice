7391a2167e88a961f5f137045e326d7289e458b2
QXu <qxu10@slb.com>
2019-07-22 23:36:31 +0800

    add some CSharp learning knowledge

---
+++ CSharp/OOADTrainning/OOADTrainning/LogSystem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOADTrainning
+{
+    public interface ILog
+    {
+        void Log();
+    }
+
+    public class MyClass
+    {
+        private ILog mlogger;
+
+        public MyClass(ILog logger)
+        {
+            mlogger = logger;
+        }
+    }
+}
